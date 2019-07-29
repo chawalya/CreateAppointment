68be8aff1aeb9a067bf4d90c29136bd4cf45da5f
Patchariya Piyaaromrat <patchariya@Patchariyas-MacBook-Pro.local>
2019-07-29 23:25:28 +0700

    ui

---
+++ CreateAppointment/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  CreateAppointment
+//
+//  Created by Patchariya Piyaaromrat on 24/7/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class Location: UICollection {
+
+}
---
+++ CreateAppointment/LocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationTableViewCell.swift
+//  CreateAppointment
+//
+//  Created by Patchariya Piyaaromrat on 25/7/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
